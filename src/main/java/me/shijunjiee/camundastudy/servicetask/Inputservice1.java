2f3cb2e0ddb69a9f3c250a6a5e3b003a4c444157
shijunjiee <shijunjiee@enn.cn>
2024-04-01 11:54:54 +0800

    测试输入输出

---
+++ src/main/java/me/shijunjiee/camundastudy/servicetask/Inputservice1.java
@@ -0,0 +1,15 @@
+package me.shijunjiee.camundastudy.servicetask;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Inputservice1 implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        System.out.println("Inputservice1 execute");
+    }
+}
---
+++ src/main/java/me/shijunjiee/camundastudy/servicetask/Inputservice2.java
@@ -0,0 +1,16 @@
+package me.shijunjiee.camundastudy.servicetask;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Inputservice2 implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        System.out.println("Inputservice2 execute");
+
+    }
+}
