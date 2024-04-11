6ddcd8e0c316194fac274f32d6b73136e210c1a9
shijunjiee <shijunjiee@enn.cn>
2024-04-11 10:53:09 +0800

    获取扩展属性测试

--- src/main/java/me/shijunjiee/camundastudy/servicetask/PrintTimeDelegate.java
+++ src/main/java/me/shijunjiee/camundastudy/servicetask/PrintTimeDelegate.java
@@ -3,8 +3,13 @@ package me.shijunjiee.camundastudy.servicetask;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
+import org.camunda.bpm.model.bpmn.instance.ServiceTask;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperties;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperty;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Date;
 
 @Component
@@ -16,5 +21,13 @@ public class PrintTimeDelegate implements JavaDelegate {
 
         execution.setVariable("aaa", "asdfadwsf");
         log.info("当前系统时间:{}", new Date());
+
+        // 获取扩展属性
+        ServiceTask serviceTask = (ServiceTask) execution.getBpmnModelElementInstance();
+        ExtensionElements extensionElements = serviceTask.getExtensionElements();
+        Collection<CamundaProperty> camundaProperties = extensionElements.getElementsQuery().filterByType(CamundaProperties.class).singleResult().getCamundaProperties();
+        camundaProperties.forEach(camundaProperty -> log.info("key:{}, value:{}", camundaProperty.getCamundaName(), camundaProperty.getCamundaValue()));
+
+        System.out.println("PrintTimeDelegate");
     }
 }
